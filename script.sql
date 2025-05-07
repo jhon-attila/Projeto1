eb8c7be17351dc3fa6a0f7e13e6a930f287cc39a
Janika <joaoattilakarpacs@gmail.com>
2025-05-07 07:44:00 -0300

    criando o script do banco

---
+++ script.sql
@@ -0,0 +1,19 @@
+-- Criando banco de dados
+create database bdExemplo;
+
+-- usando bando de dados
+
+use bdExemplo;
+
+-- criando as tabelas
+
+create table Usuarios(
+Id int primary key auto_increment,
+Nome varchar(50) not null,
+Email varchar(50) not null,
+Senha varchar(50) not null
+);
+
+-- consultando os dados do banco
+
+select * from Usuarios;
